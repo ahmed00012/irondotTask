369374f62ee5993b8f75ac5f0c6f4f25a506dacc
ahmed00012 <52130117+ahmed00012@users.noreply.github.com>
2020-02-22 16:52:33 +0200

    Add files via upload

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -3,10 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:irondot_task/Models/MyModel.dart';
 import 'package:irondot_task/WebService/ApiProvider.dart';
 import 'package:irondot_task/pages/DetailsCard.dart';
-<<<<<<< HEAD
-=======
-import 'package:photo_view/photo_view.dart';
->>>>>>> 01f0c6b59806b76d7a4ec52297c1b2c0beda3d40
 
 class HomePage extends StatefulWidget {
   @override
@@ -32,7 +28,6 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-<<<<<<< HEAD
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,15 +61,14 @@ class _HomePageState extends State<HomePage> {
                                         Image.network(
                                           e.coverPhoto,
                                           width: 200,
-
-
                                         ),
                                         Padding(
                                             padding:
                                                 EdgeInsets.only(right: 20)),
                                         InkWell(
                                             child: Column(
-                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
                                               children: <Widget>[
                                                 Padding(
                                                     padding: EdgeInsets.only(
@@ -94,39 +88,46 @@ class _HomePageState extends State<HomePage> {
                                                     Text(
                                                       "Beds : ",
                                                       style: TextStyle(
-                                                          fontSize: 16, color: Colors.deepPurple),
+                                                          fontSize: 16,
+                                                          color: Colors
+                                                              .deepPurple),
                                                     ),
                                                     Text(
                                                       e.numBed,
-                                                      style: TextStyle(fontSize: 16),
+                                                      style: TextStyle(
+                                                          fontSize: 16),
                                                     )
                                                   ],
                                                 ),
-
                                                 Row(
                                                   children: <Widget>[
                                                     Text(
                                                       "Bathroom : ",
                                                       style: TextStyle(
-                                                          fontSize: 16, color: Colors.deepPurple),
+                                                          fontSize: 16,
+                                                          color: Colors
+                                                              .deepPurple),
                                                     ),
                                                     Text(
                                                       e.numBath,
-                                                      style: TextStyle(fontSize: 16),
+                                                      style: TextStyle(
+                                                          fontSize: 16),
                                                     )
                                                   ],
                                                 ),
-
                                                 Row(
                                                   children: <Widget>[
                                                     Text(
                                                       "Parking : ",
                                                       style: TextStyle(
-                                                          fontSize: 16, color: Colors.deepPurple),
+                                                          fontSize: 16,
+                                                          color: Colors
+                                                              .deepPurple),
                                                     ),
                                                     Text(
                                                       e.numParking,
-                                                      style: TextStyle(fontSize: 16),
+                                                      style: TextStyle(
+                                                          fontSize: 16),
                                                     )
                                                   ],
                                                 ),
@@ -135,7 +136,6 @@ class _HomePageState extends State<HomePage> {
                                                     Text(
                                                       "price: ",
                                                       style: TextStyle(
-
                                                           fontSize: 16,
                                                           color: Colors
                                                               .deepPurple),
@@ -181,92 +181,6 @@ class _HomePageState extends State<HomePage> {
                                                                 .images[0]
                                                                 .toString(),
                                                           )));
-=======
-
-
-
-
-  @override
-  Widget build(BuildContext context) {
-
-
-
-
-
-
-
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue[900],
-          elevation: 0.0,
-          title: Text(
-            "Recent Hotels",
-            style: TextStyle(color: Colors.white),
-          ),
-          centerTitle: true,
-        ),
-        body: isLoading
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : ListView.builder(
-            itemCount: hotels.length,
-
-            itemBuilder: (context,index){
-              return Container(
-
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-
-                    children: hotels.map((e) => Card(
-                      child: Hero(
-                        tag: Text(e.name),
-
-                        child:Container(
-                            height: 150,
-child: Row(
-  children: <Widget>[
-    Image.network(e.coverPhoto,fit: BoxFit.fitHeight,width: 200,),
-    
-    Padding(padding: EdgeInsets.only(right: 20)),
-   InkWell(
-     child:  Column(
-       children: <Widget>[
-         Padding(padding: EdgeInsets.only(top: 20)),
-         Text(e.name,style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
-         Padding(padding: EdgeInsets.only(top: 50)),
-         Row(
-           children: <Widget>[
-             Text("price: ",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18,color: Colors.deepPurple),),
-             Text(e.price,style: TextStyle(fontWeight: FontWeight.w500),)
-           ],
-         ),
-
-       ],
-     ),
-     onTap:()=> Navigator.of(context).push( MaterialPageRoute(builder: (context)=> new DetailsCard())),
-   )
-    
-  ],
-),
-
-                        ),
-
-
-                      ),
-                    )).toList()
-                ),
-              );
-
-            }
-        ),
-
-
-
->>>>>>> 01f0c6b59806b76d7a4ec52297c1b2c0beda3d40
-
-                                              print(
-                                                  hotels[index].images.length);
                                             })
                                       ],
                                     ),
@@ -277,8 +191,5 @@ child: Row(
                 );
               }),
     );
-
-
-
   }
 }
